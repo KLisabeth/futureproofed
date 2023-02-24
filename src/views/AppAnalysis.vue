481b7a2b27c75061d33aee30ca1f0d7c47ea41b2
Sara <op.intpm1@gmail.com>
2023-02-24 17:35:47 +0100

    navigation menu setup

---
+++ src/views/AppAnalysis.vue
@@ -0,0 +1,16 @@
+<template>
+	<section id="analysis-page">
+		<h1></h1>
+		<p>This is the page</p>
+	</section>
+</template>
+
+<!-- JS  -->
+<script lang="ts">
+export default {
+	props: {},
+};
+</script>
+
+<!-- Styling  -->
+<style scoped></style>
---
+++ src/views/AppPlan.vue
@@ -0,0 +1,16 @@
+<template>
+	<section id="plan-page">
+		<h1>Plan</h1>
+		<p>This is the data page</p>
+	</section>
+</template>
+
+<!-- JS  -->
+<script lang="ts">
+export default {
+	props: {},
+};
+</script>
+
+<!-- Styling  -->
+<style scoped></style>
